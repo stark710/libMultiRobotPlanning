8653c571a1b2f17c4fee168cfb85d7c70a3bc375
Kaushik Balasundar <kaushik5498@gmail.com>
2023-04-19 15:24:39 -0400

    Baseline videos and added time-window dependent goals

--- example/next_best_assignment_mg.cpp
+++ example/next_best_assignment_mg.cpp
@@ -111,6 +111,7 @@ t_map createMultiGoalCostMatrix(std::string inputFile, NextBestAssignment<std::s
     agent_id++;
   }
 
+
     // sanity check: no identical start states
   std::unordered_set<State> startStatesSet;
   for (const auto& s : startStates) {
@@ -124,18 +125,115 @@ t_map createMultiGoalCostMatrix(std::string inputFile, NextBestAssignment<std::s
   return task_definition; 
 }
 
+// Create a time window dependent cost matrix
+
+std::vector<t_map> createMultiGoalTimedCostMatrix(std::string inputFile, NextBestAssignment<std::string, std::string> &assignment){
+  YAML::Node config = YAML::LoadFile(inputFile);
+  std::vector<t_map> task_goal_full_defintion;
+  std::unordered_set<Location> obstacles;
+  std::vector<std::unordered_set<Location> > goals;
+  std::vector<State> startStates;
+  t_map task_definition; 
+  t_map task_time_window_definition;
+  
+  const auto& dim = config["map"]["dimensions"];
+  int dimx = dim[0].as<int>();
+  int dimy = dim[1].as<int>();
+
+  for (const auto& node : config["map"]["obstacles"]) {
+    obstacles.insert(Location(node[0].as<int>(), node[1].as<int>()));
+  }
+  int agent_id=0;
+  int task_id = 0;
+  for (const auto& node : config["agents"]) {
+    std::vector<std::vector<int>> agent_potential_goals;
+    ShortestPathHeuristic m_heuristic(dimx, dimy, obstacles);
+
+    const auto& start = node["start"];
+    startStates.emplace_back(State(0, start[0].as<int>(), start[1].as<int>()));
+    goals.resize(goals.size() + 1);
+    task_id = 0;
+
+    // Iterate through potential goals and their time windows
+    for (int i=0; i<node["potentialGoals"].size(); i++){
+
+      const auto& goal = node["potentialGoals"][i];
+      const auto& time_window = node["goalTimeWindows"][i];
+      std::vector<std::vector<int>> all_goals = goal.as<std::vector<std::vector<int>>>();
+      std::vector<std::vector<int>> all_time_windows = time_window.as<std::vector<std::vector<int>>>();
+      int num_goals = all_goals.size();
+      task_definition[std::to_string(task_id)] = all_goals;
+      task_time_window_definition[std::to_string(task_id)] = all_time_windows;
+      std::vector<int> goal_last_element = goal.as<std::vector<std::vector<int>>>().back();
+      
+      // Find sum of costs to all goals 
+      int sum_cost = 0;
+      int curr_cost = 0;
+      sum_cost = m_heuristic.getValue(Location(startStates[agent_id].x,startStates[agent_id].y), Location(all_goals[0][0], all_goals[0][1]));
+
+      for (int j=1; j<all_goals.size(); j++){
+
+          int prev_goal_x = all_goals[j-1][0];
+          int prev_goal_y = all_goals[j-1][1];
+          int curr_goal_x = all_goals[j][0];
+          int curr_goal_y = all_goals[j][1];
+          curr_cost = m_heuristic.getValue(Location(prev_goal_x, prev_goal_y), Location(curr_goal_x, curr_goal_y));
+          sum_cost += curr_cost;
+      }
+
+      // If the sum of costs is less than the time window, then the agent can reach all goals in time
+      int last_goal_deadline = all_time_windows[num_goals-1][1];
+      if (sum_cost < last_goal_deadline){
+        assignment.setCost(std::to_string(agent_id), std::to_string(task_id), sum_cost);
+      }
+
+      // If the sum of costs is greater than the time window, then the agent cannot reach all goals in time
+      else{
+        assignment.setCost(std::to_string(agent_id), std::to_string(task_id), 1000000);
+      }
+      
+      task_id++;
+    }
+
+    agent_id++;
+  }
+
+
+  // sanity check: no identical start states
+  std::unordered_set<State> startStatesSet;
+  for (const auto& s : startStates) {
+    if (startStatesSet.find(s) != startStatesSet.end()) {
+      std::cout << "Identical start states detected -> no solution!" << std::endl;
+      return {};
+    }
+    startStatesSet.insert(s);
+  }
+
+  task_goal_full_defintion.push_back(task_definition);
+  task_goal_full_defintion.push_back(task_time_window_definition);
+
+  return task_goal_full_defintion; 
+}
+
+
 int main(int argc, char* argv[]) {
   namespace po = boost::program_options;
   // Declare the supported options.
   po::options_description desc("Allowed options");
   std::string inputFile;
-  std::string outputFile;
+  std::string outputFile1;
+  std::string outputFile2;
+
   desc.add_options()("help", "produce help message")(
-      "input,i", po::value<std::string>(&inputFile)->default_value("../benchmark/custom/mapfta1.yaml"),
-      "input cost (txt)")("output,o",
-                          po::value<std::string>(&outputFile)->default_value(
-                               "output_ecbsta.yaml"),
-                          "output file (YAML)");
+      "input,i", po::value<std::string>(&inputFile)->default_value("../benchmark/custom/mapfta3.yaml"),
+      "input cost (txt)")("output-1,o",
+                          po::value<std::string>(&outputFile1)->default_value(
+                               "output_ecbsta_untimed.yaml"),
+                          "output file (YAML)"),("output-2,o",
+                          po::value<std::string>(&outputFile2)->default_value(
+                               "output_ecbsta_timed.yaml"),
+                          "output file (YAML)") ;
+
 
   try {
     po::variables_map vm;
@@ -152,59 +250,19 @@ int main(int argc, char* argv[]) {
     return 1;
   }
 
-
-  // std::ifstream input(inputFile);
-  // std::regex re("(\\w+)\\s*->\\s*(\\w+)\\s*:\\s*(\\d+)");
-  // for (std::string line; getline(input, line);) {
-  //   std::smatch match;
-  //   if (std::regex_search(line, match, re) && match.size() == 4) {
-  //     std::string agent = match.str(1);
-  //     std::string task = match.str(2);
-  //     int cost = std::stoi(match.str(3));
-  //     assignment.setCost(agent, task, cost);
-  //   } else {
-  //     std::cerr << "Couldn't match line \"" << line << "\"!" << match.size()
-  //               << std::endl;
-  //   }
-  // }
-
-  // const size_t numAgents = 4;
-  // const size_t numTasks = 4;
-
-  // const int64_t cost[numAgents][numTasks] = {{90, 76, 75, 80},
-  //                                            {35, 85, 55, 65},
-  //                                            {125, 95, 90, 105},
-  //                                            {45, 110, 95, 115}};
-
-  // for (size_t i = 0; i < numAgents; ++i) {
-  //   for (size_t j = 0; j < numTasks; ++j) {
-  //     a.setCost("a" + std::to_string(i), "t" + std::to_string(j),
-  //     cost[i][j]);
-  //   }
-  // }
   std::map<std::string, std::string> solution;
-  NextBestAssignment<std::string, std::string> assignment;
-  t_map task_definition = createMultiGoalCostMatrix(inputFile, assignment);
-  assignment.solve(task_definition);
-  assignment.nextSolution(solution, task_definition);
+  NextBestAssignment<std::string, std::string> assignment_1;
+  NextBestAssignment<std::string, std::string> assignment_2;
+  t_map task_definition = createMultiGoalCostMatrix(inputFile, assignment_1);
+  std::vector<t_map> task_time_window_definition = createMultiGoalTimedCostMatrix(inputFile, assignment_2);
+  
+  // Solve the assignment problem untimed 
+  // assignment_1.solve(task_definition);
+  // assignment_1.nextSolution(solution, task_definition);
 
-  std::ofstream out(outputFile);
-  out << "solutions:" << std::endl;
-
-  for (size_t i = 0;; ++i) {
-    int64_t c = assignment.nextSolution(solution);
-    if (solution.empty()) {
-      if (i == 0) {
-        out << "  []" << std::endl;
-      }
-      break;
-    }
-    out << "  - cost: " << c << std::endl;
-    out << "  - assignment:" << std::endl;
-    for (const auto& s : solution) {
-      out << "      " << s.first << ": " << s.second << std::endl;
-    }
-  }
+  // Solve the assignment problem timed
+  assignment_2.solve(task_time_window_definition[0]);
+  assignment_2.nextSolution(solution, task_time_window_definition[0]);
 
   return 0;
 }
